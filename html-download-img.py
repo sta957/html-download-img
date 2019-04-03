f54f6348024784cf1a5e8d3f2bb6f810cb85d7dc
Timo S <tstaudinger@live.de>
2019-04-04 00:32:18 +0200

    adding multi-processing + bugfixes

--- html-download-img.py
+++ html-download-img.py
@@ -11,85 +11,130 @@ import urllib.request
 import requests
 import sys
 import os
+import multiprocessing
 
 
 
-url_to_scrape = 'http://' + sys.argv[2] # combine 'http://' with the given argument of cli
-dir_path = sys.argv[1]                  # the directory is named by it's site
-headers = {                             # set the useragent to appear as real user
+url_to_scrape = 'http://' + sys.argv[2]                 # combine 'http://' with the given argument of cli
+dir_path = sys.argv[1]                                  # the directory is named by it's site
+headers = {                                             # set the useragent to appear as real user
     'User-Agent':'Mozilla/5.0 (Windows NT 6.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
 }
-page = requests.get(url_to_scrape, headers = headers) #, headers = headers)      # set a get-request to the webpage
-page_content = page.content             # get the html-content of the get-request
-soup = BeautifulSoup(page_content, 'lxml')      # store the html-content into the soup variable
+page = requests.get(url_to_scrape, headers = headers)   # set a get-request to the webpage
+page_content = page.content                             # get the html-content of the get-request
+soup = BeautifulSoup(page_content, 'lxml')              # store the html-content into the soup variable
 img_num = 0
 x = 0
 y = 0
-img_elements = soup.find_all('img')
+img_elements = soup.find_all('img')                     # find all images in soup variable
 
-# create a local folder to store the images from html-site
-# folders with the same name have to be deleted first to avoid an error
-try:
-    os.mkdir(str(dir_path))
-except:
-    print("Directory " + str(dir_path) + " can't be created!")
-else:
-    print("Directory " + str(dir_path) + " successfully created!")
+# define func that verifies input for y/N
+def folder_wr():
+    write_req = input('Do you want to write into existing folder ' + str(dir_path) + ' (y/N)? [y]: ')
+    if write_req == '':
+        write_req = 'y'
+    elif write_req != 'y' and write_req != 'N':
+        print('Please enter a valid argument...')
+        folder_wr()
+    return write_req
 
+# if not exits create a local folder to store the images from html-site
+# else ask to put images into the existing folder
+def folder_check():
+    if os.path.isdir(dir_path) == False:
+        try:
+            os.mkdir(str(dir_path))
+        except:
+            print("Directory " + str(dir_path) + " can't be created!")
+        else:
+            print("Directory " + str(dir_path) + " successfully created!")
+    else:
+        if folder_wr() == 'N':
+            exit()
 
 # for all images on html-site
-for i in img_elements:
-    raw_url = i.get('src')
+def get_img(img_elements_f):
+    img_num = 0
+    for i in img_elements_f:
+        if i.get('data-src') is not None:
+            raw_url = i.get('data-src')
+        else:
+            raw_url = i.get('src')
 
-    # skip img with data: tag
-    if raw_url[:5] == "data:":
-        continue
+        # skip img with data: tag
+        if raw_url[:5] == "data:":
+            continue
 
-    # check if it's the full url-path or just a subpath
-    if raw_url[:4] == "http":
-        image_url = raw_url
-    elif raw_url[:3] == "www":
-        image_url = raw_url
-    elif raw_url[:1] == "/":
-        image_url = url_to_scrape + raw_url
-    else:
-        image_url = url_to_scrape + '/' + raw_url
+        # check if it's the full url-path or just a subpath
+        if raw_url[:4] == "http":
+            image_url = raw_url
+        elif raw_url[:3] == "www":
+            image_url = raw_url
+        elif raw_url[:2] == "//":
+            image_url = 'http:' + raw_url
+        elif raw_url[:1] == "/":
+            image_url = url_to_scrape + raw_url
+        else:
+            image_url = url_to_scrape + '/' + raw_url
 
-    # get the title of the requested image
-    img_header = i.get('alt')
+        # set the url as image-name
+        filename_complete_url = image_url.split('/')
+        filename_url = str(filename_complete_url[-1]).split('.')
+        filename = str(filename_url[0])
+        img_type = str(filename_url[1]).split('?')
 
-    # check if title of image is a NoneType -> set header
-    if img_header is None:
-        print('NoneType detected: ' + str(image_url))
-        img_header = 'NT_' + str(y)
-        y = y+1
+        # increase image counter and get the image
+        img_num = img_num+1
+        req_img_url = requests.get(image_url)
 
-    if img_header != '' and '<' not in img_header and '>' not in img_header:
-        filename = img_header
-    else:
-        filename = str(x)
-        x = x+1
-
-    # increase image counter and get the image
-    img_num = img_num+1
-    req_img_url = requests.get(image_url)
-
-    # store the image files in the right format
-    if image_url[-4:] == '.png':
-        with open(str(dir_path) + '/' + str(filename) + '.png', 'wb') as f:
-            f.write(req_img_url.content)
-    elif image_url[-4:] == '.gif':
-        with open(str(dir_path) + '/' + str(filename) + '.gif', 'wb') as f:
-            f.write(req_img_url.content)
-    elif image_url[-4:] == '.svg':
-            with open(str(dir_path) + '/' + str(filename) + '.svg', 'wb') as f:
-                f.write(req_img_url.content)
-    else:
-        with open(str(dir_path) + '/' + str(filename) + '.jpeg', 'wb') as f:
+        # store the image files
+        with open(str(dir_path) + '/' + str(filename) + '.' + str(img_type[0]), 'wb') as f:
             f.write(req_img_url.content)
 
-    print(image_url) # output for debugging purposes
+        print(image_url)
 
+def testf(num):
+    return num+10
 
+# worker thread function
+def worker(num, num_work, c_elem):
+    # check for usecase of thread
+    e_inspect = 0
+    to_inspect = []
 
-print("Code was completed! Downloaded " + str(img_num) + " images into folder: " + str(dir_path))
+    if c_elem >= num_work:
+        e_inspect = round(c_elem / num_work)                   # number of elements to inspect by simple worker
+        e_range_start = num * e_inspect                 # first element to be scanned
+        e_range_end = (e_range_start + e_inspect) - 1   # last element to be scanned
+    elif c_elem >= num + 1:
+        e_inspect = 1
+        e_range_start = num
+        e_range_end = num
+    else:
+        exit()
+
+    # check for the last thread to scan the lasting images
+    if num + 1 == num_work:
+        e_range_start = c_elem - e_inspect
+        e_range_end = c_elem - 1
+
+    for x in range(e_range_start, e_range_end + 1):
+        to_inspect.append(img_elements[x])
+
+    get_img(to_inspect) ## fix ths bugs
+
+    # print ('Worker:' + str(num) + ' --> '+ str(to_inspect)) # for debugging purposes
+
+# multiprocessing the download
+def multiproc():
+    if __name__ == '__main__':
+        jobs = []
+        range_start = 0
+        count_elements = len(img_elements)
+        num_workers = 15
+        for i in range(num_workers):
+            p = multiprocessing.Process(target=worker, args=(i, num_workers, count_elements))
+            jobs.append(p)
+            p.start()
+
+multiproc()
